60b05dacfccb1b70aca501a04ab42163e95ace74
giannfc <casnatigianfranco@gmail.com>
2023-02-05 02:22:27 -0300

    upload

---
+++ GFC Portfolio/js/script.js
@@ -0,0 +1,13 @@
+/*var check=document.querySelector(".check");
+check.addEventListener('click',language);
+
+function language(){
+    console.log(check.checked);
+
+    let id=check.checked;
+    if (id==true){
+        location.href=href="es/index.html";
+    }else{
+        location.href="../index.html";
+    }
+}*/
